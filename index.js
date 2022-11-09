c285fbfeed9e800b4973506a2cc140c1ab1260d4
Imam-Hossain <zunayedemon6t9@gmail.com>
2022-11-09 20:11:31 +0600

    added middle wares

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middle wares
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
